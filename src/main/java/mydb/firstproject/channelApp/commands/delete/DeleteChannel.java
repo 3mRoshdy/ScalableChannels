e33a18dd849d200e8d9bb25134549ee39879557b
3mRoshdy <ahmedhroshdy@gmail.com>
2018-03-13 09:30:06 +0200

    Added POST, DELETE Command infrastructure

---
+++ src/main/java/mydb/firstproject/channelApp/commands/delete/DeleteChannel.java
@@ -0,0 +1,10 @@
+package mydb.firstproject.channelApp.commands.delete;
+
+import mydb.firstproject.channelApp.commands.Command;
+
+public class DeleteChannel extends Command{
+    @Override
+    protected void execute() {
+
+    }
+}
---
+++ src/main/java/mydb/firstproject/channelApp/commands/post/PostChannel.java
@@ -0,0 +1,10 @@
+package mydb.firstproject.channelApp.commands.post;
+
+import mydb.firstproject.channelApp.commands.Command;
+
+public class PostChannel extends Command {
+    @Override
+    protected void execute() {
+        System.out.println("Executing Post");
+    }
+}
